d67d69fb8cf11eb75160e7228cfec6ea2f32e15e
LaurenRolan <lauren.rolan@ecole.ensicaen.fr>
2019-04-05 11:57:37 +0200

    Login servlet working

--- TennisClub/src/servlet/ActionServlet.java
+++ TennisClub/src/servlet/ActionServlet.java
@@ -1,11 +1,5 @@
 package servlet;
 
-import beanEntity.AdherentEntity;
-
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-import javax.persistence.Query;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -14,7 +8,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.List;
 
 @WebServlet(name = "actionServlet", urlPatterns = {"/action"})
 public class ActionServlet extends HttpServlet {
@@ -27,7 +20,8 @@ public class ActionServlet extends HttpServlet {
         PrintWriter out = resp.getWriter();
 
         String email = req.getParameter("email"),
-                pass = req.getParameter("password");
+                pass = req.getParameter("password"),
+                action = req.getParameter("code");
 
         out.write("<html><body><div id='info' style='text-align: center;'>");
 
@@ -40,26 +34,12 @@ public class ActionServlet extends HttpServlet {
             rdObj = req.getRequestDispatcher("/index.jsp");
             rdObj.include(req, resp);
         } else {
-            String hashed = CryptoFunction.encryptThisString(pass);
-            try {
-                EntityManagerFactory emf = Persistence.createEntityManagerFactory("TennisUnit");
-                EntityManager em = emf.createEntityManager();
-                System.out.println("Created em");
-
-                Query query = em.createQuery("from AdherentEntity adherent where adherent.email= :email");
-                query.setParameter("email", email);
-                System.out.println(query.toString());
-                List results = query.getResultList();
-                AdherentEntity adherentEntity = null;
-                if (!results.isEmpty())
-                    adherentEntity = (AdherentEntity) results.get(0);
-
-                System.out.println(adherentEntity.getNom() + " " + adherentEntity.getPrenom());
-
-            } catch (Exception e) {
-                System.out.println("Didn't work");
-                e.printStackTrace();
+            if(action.equals("L")) {
+                RequestDispatcher dispatcher = getServletContext()
+                        .getRequestDispatcher("/login");
+                dispatcher.forward(req, resp);
             }
+            System.out.println(action);
         }
         out.write("</div></body></html>");
         out.close();
--- TennisClub/src/servlet/LoginServlet.java
+++ TennisClub/src/servlet/LoginServlet.java
@@ -1,19 +1,55 @@
 package servlet;
 
+import beanEntity.AdherentEntity;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
-@WebServlet(name = "LoginServlet")
+//https://openclassrooms.com/fr/courses/626954-creez-votre-application-web-avec-java-ee/622596-la-session-connectez-vos-clients
+
+@WebServlet(name = "LoginServlet",  urlPatterns = {"/login"})
 public class LoginServlet extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest req, HttpServletResponse response) throws ServletException, IOException {
+        String email = req.getParameter("email"),
+                pass = req.getParameter("password");
 
+        String hashed = CryptoFunction.encryptThisString(pass);
+        try {
+            EntityManagerFactory emf = Persistence.createEntityManagerFactory("TennisUnit");
+            EntityManager em = emf.createEntityManager();
+            System.out.println("Created em");
+
+            Query query = em.createQuery("from AdherentEntity adherent where adherent.email= :email");
+            query.setParameter("email", email);
+            System.out.println(query.toString());
+            List results = query.getResultList();
+            AdherentEntity adherentEntity = null;
+            if (!results.isEmpty()) { //The email exists
+                adherentEntity = (AdherentEntity) results.get(0);
+                if(hashed.equals(adherentEntity.getPassword())) {
+                    System.out.println("Success");
+                } else {
+                    System.out.println("Different hash");
+                }
+            }
+            else {
+
+            }
+            System.out.println(adherentEntity.getNom() + " " + adherentEntity.getPrenom());
+
+        } catch (Exception e) {
+            System.out.println("Didn't work");
+            e.printStackTrace();
+        }
     }
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-    }
 }
